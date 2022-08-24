7ae44eefabe972012fc15143cada72c4c815a7e0
Jan Eerik <janeerik@gmail.com>
2022-08-25 00:37:15 +0300

    App ready

---
+++ can_cat_food.py
@@ -0,0 +1,33 @@
+import sqlite3
+from datetime import datetime
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+def get_db_connection():
+    conn = sqlite3.connect("database.sqlite")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    conn = get_db_connection()
+    query = "SELECT * FROM meals ORDER BY last_meal DESC"
+    if request.method == "GET":
+        last_meal = conn.execute(query).fetchone()
+        conn.close()
+
+        return render_template("index.html", last_meal=last_meal)
+
+    now = datetime.strftime(datetime.now(), "%d-%m-%Y %H:%M:%S")
+    conn.execute("INSERT INTO meals (last_meal) VALUES (?)", (now,))
+    last_meal = conn.execute(query).fetchone()
+    conn.close()
+
+    return render_template("index.html", last_meal=last_meal)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
